e864ac78cc2fc5e8990dec4ea58531da370a518f
arshadsid <sidd.arshad916@gmail.com>
2018-06-19 23:36:04 +0530

    Skeletonisation try 1 (improvement)

--- new_code/headers/edge_thinning.cpp
+++ new_code/headers/edge_thinning.cpp
@@ -179,6 +179,56 @@ bool Remove_External_Pixel2 (Point point, Mat_<bool>& mask)
     return true;
 }
 
+// Remove an external pixel
+bool Remove_External_Pixel_Iterative (Point point, Mat_<bool>& mask, bool& check)
+{
+    // exceptional cases
+    if ( point.x < 0 or point.y < 0 or point.x >= mask.cols or point.y >= mask.rows ) return false;
+    if ( Remove_Isolated_Pixel( point, mask ) ) return true;
+    // corners aren't removed
+    if ( point == Point(0,0) ) return false;
+    if ( point == Point(0,mask.rows-1) ) return false;
+    if ( point == Point(mask.cols-1,0) ) return false;
+    if ( point == Point(mask.cols-1,mask.rows-1) ) return false;
+    //bool debug = false; if ( point.x == mask.cols-1 ) debug = true;
+    int num_neighbors;
+    Point nbrs[8];
+    std::vector<bool> neighbors( shifts8.size(), true );
+    Neighbors2( mask, point, shifts8, num_neighbors, neighbors, nbrs );
+    int changes = 0;
+    for ( int i = 0; i < shifts8.size(); i++ )
+        if ( neighbors[i] != neighbors[ (i+1) % neighbors.size() ] ) changes++;
+    //if ( debug ) std::cout<<"\n"<<point<<" n="<<num_neighbors<<" c="<<changes;
+    if ( num_neighbors > 3 and num_neighbors < 6 and changes == 2)
+    {
+
+        bool connectivity = true;
+        for(int cn = 0; cn < num_neighbors; cn++)
+        {
+            std::vector<bool> nbrs_neighbors( shifts8.size(), true );
+            int num_nbrs_neighbors;
+            Point nbrs_nbrs[8];
+            Neighbors2( mask, nbrs[cn], shifts8, num_nbrs_neighbors, nbrs_neighbors, nbrs_nbrs );
+            int connected = 0;
+            for(int cnn = 0; cnn < num_nbrs_neighbors; cnn++)
+            {
+                for(int cnnn = 0; cnnn < num_neighbors; cnnn++)
+                {
+                    if (nbrs_nbrs[cnn] == nbrs[cnnn])
+                        connected++;
+                }
+            }
+            if(connected == 0 )connectivity = false;
+        }
+        if(connectivity)
+        {
+            mask.at<bool>( point ) = false;
+            check = true;
+        }
+    }
+    return true;
+}
+
 
 // Function removes all external pixels
 bool Remove_External_Pixels2 (Mat_<bool>& mask)
@@ -198,6 +248,44 @@ bool Remove_External_Pixels (Mat_<bool>& mask)
         for ( int col = 0; col < mask.cols; col++ )
             if ( mask.at<bool>( Point( col, row ) ) )
                 Remove_External_Pixel( Point( col, row ), mask );
+    return true;
+}
+
+
+// Function removes all external pixels
+bool Remove_External_Pixels_Iterative (Mat_<bool>& mask)
+{
+    bool check;
+    do{
+        check = false;
+        for ( int row = 0; row < mask.rows; row++ )
+            for ( int col = 0; col < mask.cols; col++ )
+                if ( mask.at<bool>( Point( col, row ) ) )
+                    Remove_External_Pixel_Iterative( Point( col, row ), mask, check);
+    }while(check);
+
+    return true;
+}
+
+
+
+// Function removes all additional junction pixels
+bool Remove_Extra_Junctions (Mat_<bool>& mask)
+{
+    for ( int row = 0; row < mask.rows; row++ )
+        for ( int col = 0; col < mask.cols; col++ )
+            if ( mask.at<bool>( Point( col, row ) ) )
+            {
+                Point point = Point( col, row );
+                int num_neighbors;
+                std::vector<bool> neighbors( shifts8.size(), true );
+                Neighbors( mask, point, shifts8, num_neighbors, neighbors );
+                int changes = 0;
+                for ( int i = 0; i < shifts8.size(); i++ )
+                    if ( neighbors[i] != neighbors[ (i+1) % neighbors.size() ] ) changes++;
+                if (num_neighbors > 2 and changes == 2)
+                    mask.at<bool>( point ) = false;
+            }
     return true;
 }
 
@@ -235,34 +323,34 @@ bool Add_Internal_Pixels (Mat_<bool>& mask)
 
 bool Pixels_to_Graph (Mat_<bool>const& mask, bool object, Graph& graph)
 {
-graph.clear();
-std::map< Point, Graph::vertex_descriptor, Compare_Points > pixels_vertices;
-// Add vertices
-for ( int row = 0; row < mask.rows; row++ )
-for ( int col = 0; col < mask.cols; col++ )
-{
-if ( mask.at<bool>( row, col ) != object ) continue; // irrelevant pixel
-auto vertex = boost::add_vertex( graph );
-graph[ vertex ] = Point( col, row );
-pixels_vertices.insert( std::make_pair( Point( col, row ), vertex ) );
-}
-// Add horizontal edges
-for ( int row = 0; row < mask.rows; row++ )
-for ( int col = 0; col+1 < mask.cols; col++ )
-{
-Point p0( col, row ), p1(  col+1, row );
-if ( mask.at<bool>( p0 ) != object or mask.at<bool>( p1 ) != object ) continue;
-boost::add_edge( pixels_vertices[ p0 ], pixels_vertices[ p1 ], graph );
-}
-// Add vertical edges
-for ( int row = 0; row+1 < mask.rows; row++ )
-for ( int col = 0; col < mask.cols; col++ )
-{
-Point p0( col, row ), p1(  col, row+1 );
-if ( mask.at<bool>( p0 ) != object or mask.at<bool>( p1 ) != object ) continue;
-boost::add_edge( pixels_vertices[ p0 ], pixels_vertices[ p1 ], graph );
-}
-return true;
+    graph.clear();
+    std::map< Point, Graph::vertex_descriptor, Compare_Points > pixels_vertices;
+    // Add vertices
+    for ( int row = 0; row < mask.rows; row++ )
+        for ( int col = 0; col < mask.cols; col++ )
+        {
+            if ( mask.at<bool>( row, col ) != object ) continue; // irrelevant pixel
+            auto vertex = boost::add_vertex( graph );
+            graph[ vertex ] = Point( col, row );
+            pixels_vertices.insert( std::make_pair( Point( col, row ), vertex ) );
+        }
+    // Add horizontal edges
+    for ( int row = 0; row < mask.rows; row++ )
+        for ( int col = 0; col+1 < mask.cols; col++ )
+        {
+            Point p0( col, row ), p1(  col+1, row );
+            if ( mask.at<bool>( p0 ) != object or mask.at<bool>( p1 ) != object ) continue;
+            boost::add_edge( pixels_vertices[ p0 ], pixels_vertices[ p1 ], graph );
+        }
+    // Add vertical edges
+    for ( int row = 0; row+1 < mask.rows; row++ )
+        for ( int col = 0; col < mask.cols; col++ )
+        {
+            Point p0( col, row ), p1(  col, row+1 );
+            if ( mask.at<bool>( p0 ) != object or mask.at<bool>( p1 ) != object ) continue;
+            boost::add_edge( pixels_vertices[ p0 ], pixels_vertices[ p1 ], graph );
+        }
+    return true;
 }
 
 
@@ -275,9 +363,9 @@ bool Is_Boundary (Point p, Point size)
 
 bool Is_Boundary (std::vector<Point>const& points, Point size)
 {
-for ( auto p : points )
-if ( Is_Boundary( p, size ) ) return true;
-return false;
+    for ( auto p : points )
+        if ( Is_Boundary( p, size ) ) return true;
+    return false;
 }
 
 // Remove all small components
@@ -340,32 +428,34 @@ Mat Save_Mask (Mat const& image, Mat const& mask, std::string name)
 
 bool Boundary (Mat_<bool>const& mask, std::vector<bool>& boundary)
 {
-int row = 0, col = 0;
-for ( row = 0, col = 0; row < mask.rows; row++ ) boundary.push_back( mask.at<bool>( row, col ) );
-for ( row = mask.rows-1, col = 0; col < mask.cols; col++ ) boundary.push_back( mask.at<bool>( row, col ) );
-for ( row = mask.rows-1, col = mask.cols-1; row >= 0; row-- ) boundary.push_back( mask.at<bool>( row, col ) );
-for ( row = 0, col = mask.cols-1; col >= 0; col-- ) boundary.push_back( mask.at<bool>( row, col ) );
-return true;
+    int row = 0, col = 0;
+    for ( row = 0, col = 0; row < mask.rows; row++ ) boundary.push_back( mask.at<bool>( row, col ) );
+    for ( row = mask.rows-1, col = 0; col < mask.cols; col++ ) boundary.push_back( mask.at<bool>( row, col ) );
+    for ( row = mask.rows-1, col = mask.cols-1; row >= 0; row-- ) boundary.push_back( mask.at<bool>( row, col ) );
+    for ( row = 0, col = mask.cols-1; col >= 0; col-- ) boundary.push_back( mask.at<bool>( row, col ) );
+    return true;
 }
 
 
 // Count boundary vertices
 bool Boundary_Vertices (Mat_<bool>const& mask, int& boundary_vertices)
 {
-boundary_vertices = 0;
-std::vector<bool> boundary;
-Boundary( mask, boundary );
-int changes = 0;
-for ( int i = 0; i < boundary.size(); i++ )
-if ( boundary[i] != boundary[ (i+1) % boundary.size() ] ) changes++;
-if ( changes % 2 != 0 )
-{
-std::cout<<"\nError in Remove_Small_Components"<< std::endl;
-return false;
+    boundary_vertices = 0;
+    std::vector<bool> boundary;
+    Boundary( mask, boundary );
+    int changes = 0;
+    for ( int i = 0; i < boundary.size(); i++ )
+        if ( boundary[i] != boundary[ (i+1) % boundary.size() ] ) changes++;
+            if ( changes % 2 != 0 )
+            {
+                std::cout<<"\nError in Remove_Small_Components"<< std::endl;
+                return false;
+            }
+    boundary_vertices = int( changes / 2 );
+    return true;
 }
-boundary_vertices = int( changes / 2 );
-return true;
-}
+
+
 
 
 /****************************************|| EDGE_THINNING FUNCTION STARTS HERE ||***************************************
@@ -384,7 +474,7 @@ bool Edge_Thinning (Mat const& image, Mat_<bool>& mask, std::vector<int>& min_ar
 {
     // Smooth boundary contours
     Remove_External_Pixels( mask );
-    if ( save_images ) Save_Mask( image, mask, name + "_RE.png" );
+    //if ( save_images ) Save_Mask( image, mask, name + "_RE.png" );
     Add_Internal_Pixels( mask );
     if ( save_images ) Save_Mask( image, mask, name + "_AI.png" );
 
@@ -427,26 +517,27 @@ bool Edge_Thinning (Mat const& image, Mat_<bool>& mask, std::vector<int>& min_ar
     //num_vertices[i] = 2*num_domains[i] - boundary_vertices - 1;
     //std::cout<<" ("<< 100 << "," << num_domains[i] << "," <<num_vertices[i] <<")"<< std::endl;
 
-    Remove_External_Pixels2( mask );
-    if ( save_images ) Save_Mask( image, mask, name + "_output.png" );
+    Remove_External_Pixels_Iterative( mask );
+    //if ( save_images ) Save_Mask( image, mask, name + "_intermediate_skeleton.png" );
 
-    Mat_<Vec3b> image_mask( image.size(), white );
-    for ( int row = 0; row < mask.rows; row++ )
-        for ( int col = 0; col < mask.cols; col++ )
-            image_mask.at<Vec3b>( row, col ) = image.at<Vec3b>( row, col );
 
-    for ( int row = 0; row < mask.rows; row++ )
-        for ( int col = 0; col < mask.cols; col++ )
-            if ( mask.at<bool>( row, col ) )
-                image_mask.at<Vec3b>( row, col ) = black;
+    Remove_Extra_Junctions(mask);
+    if ( save_images ) Save_Mask( image, mask, name + "_skeleton.png" );
 
-    cv::imwrite(  name + "_output1.png" , image_mask );
+    if ( save_images ){
+        Mat_<Vec3b> image_mask( image.size(), white );
+        for ( int row = 0; row < mask.rows; row++ )
+            for ( int col = 0; col < mask.cols; col++ ){
+                image_mask.at<Vec3b>( row, col ) = image.at<Vec3b>( row, col );
+                if ( mask.at<bool>( row, col ) ){
+                    image_mask.at<Vec3b>( row, col ) = black;
+                    im.at<Vec3b>( row, col ) = black;
+                }
+        }
 
-    for ( int row = 0; row < mask.rows; row++ )
-        for ( int col = 0; col < mask.cols; col++ )
-            if ( mask.at<bool>( row, col ) )
-                im.at<Vec3b>( row, col ) = black;
+        cv::imwrite(  name + "_skeleton_original.png" , image_mask );
+        cv::imwrite(  name + "_skeleton_no_holes.png" , im );
+    }
 
-    cv::imwrite(  name + "_output2.png" , im );
     return true;
 }
