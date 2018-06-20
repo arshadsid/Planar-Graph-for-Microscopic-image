1dd2d9a91471df42c30ce3d2ed1b76a1d03d23d8
Zheng Sun <619444287@qq.com>
2018-06-20 12:34:46 +0100

    Update edge_thinning.cpp

--- new_code/headers/edge_thinning.cpp
+++ new_code/headers/edge_thinning.cpp
@@ -362,7 +362,7 @@ bool Pixels_to_Graph (Mat_<bool>const& mask, bool object, Graph& graph)
         }
     //Add diagonal edges from top-right
     for (int row = 0; row < mask.rows - 1; row++)
-        for(int col = mask.cols-1 ; col > 1; col--){
+        for(int col = mask.cols-1 ; col > 0; col--){
             Point p0(col, row), p1(col - 1, row), p2(col - 1, row + 1), p3(col, row + 1);
             //NOT add diagonal edges if already connected.
             if ( mask.at<bool>( p0 ) != object or mask.at<bool>( p2 ) != object or mask.at<bool>(p1 ) == object or mask.at<bool>(p3 ) == object) continue;
